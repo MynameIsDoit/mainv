e6c122206519f7235a1610e12c7483f109d45dbd
DOIT <100814449+MynameIsDoit@users.noreply.github.com>
2025-07-23 17:29:54 +0700

    Create Gamelist.lua

---
+++ Gamelist.lua
@@ -0,0 +1,6 @@
+local Games = {
+  [129827112113663] = "https://raw.githubusercontent.com/AhmadV99/Script-Games/main/Blox%20Fruit.lua", -- Blox Fruit Sea 1
+  [4442272183] = "https://raw.githubusercontent.com/AhmadV99/Script-Games/main/Blox%20Fruit.lua", -- Blox Fruit Sea 2
+}
+
+return Games
